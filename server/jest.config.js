e3ee756fab821ea294572572b326c6294fa3e712
andram11 <mertilosandra@gmail.com>
2024-11-08 07:51:40 +0100

    Added tests for addresses service & route

---
+++ server/jest.config.js
@@ -0,0 +1,23 @@
+// jest.config.js
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
+    testMatch: ['**/tests/**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js'],
+    coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/utils/dbClient.ts', // Ignore dbClient file
+  ],
+   
+  };
+  
